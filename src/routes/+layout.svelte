1bc13259d502638b31d35b73c6a6e6ea881d4a0f
PenPow <josh@penpow.dev>
2023-03-04 15:20:34 +0000

    feat: rendering + navigation

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<style>
+	:root {
+		overflow-y: scroll;
+		scrollbar-width: none;
+		-ms-overflow-style: none;
+	}
+
+	:root::-webkit-scrollbar {
+		width: 0;
+		height: 0;
+	}
+</style>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKits</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import type { PageData } from "./$types";
+	export let data: PageData;
+
+	let slideNumber = 0;
+
+	const slides = data.out.split('<hr />');
+	let slide = slides[slideNumber];
+
+	const KEYBINDS: Record<string, string[]> = {
+		left: ['KeyA', 'KeyS', 'KeyP', 'KeyH', 'KeyK'],
+		right: ['Enter', 'Space', 'KeyW', 'KeyD', 'KeyN', 'KeyJ', 'KeyL']
+	}
+
+	function changeSlide(event: KeyboardEvent) {
+		if(KEYBINDS.left.includes(event.code)) slideNumber = (slideNumber === 0 ? 0 : slideNumber - 1)
+		else if(KEYBINDS.right.includes(event.code)) slideNumber = (slideNumber === (slides.length - 1) ? slideNumber : slideNumber + 1)
+
+		slide = slides[slideNumber];
+	}
+</script>
+
+<svelte:window on:keypress|preventDefault={changeSlide}/>
+
+{@html slide}
